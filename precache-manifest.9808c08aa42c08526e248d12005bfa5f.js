86263acc5b9e5e3850be5d64cc3b512fe6f669e7
unknown <lutskyimakar@gmail.com>
2020-10-25 12:16:08 +0200

    Updates

---
+++ precache-manifest.9808c08aa42c08526e248d12005bfa5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b25ce4c2a0033c13e3010a541af85272",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "fa7eb5500d4111949957",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.364f0683.chunk.css"
+  },
+  {
+    "revision": "eed62155c168079fe5e1",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.c54cdf43.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.c54cdf43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa7eb5500d4111949957",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.786dc23f.chunk.js"
+  },
+  {
+    "revision": "356906f07e3d724d1528",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.d720b385.js"
+  }
+]);
